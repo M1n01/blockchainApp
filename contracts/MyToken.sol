3093833d2a71c43cc54470cce028e82ca6a3af81
M1n01 <abe.minato.bz@gmail.com>
2024-05-14 22:46:12 +0900

    chore: MyToken.solにコメント追加

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -17,24 +17,32 @@ contract MyToken {
 	// トークンの所有者のアドレスと所有量を管理
 	mapping (address => uint256) balances;
 
+	// event定義
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	constructor() {
+		// コンストラクト作成者に最大供給量のトークンを設定
 		balances[msg.sender] = totalSupply;
+		// オーナーをコントラクト作成者に設定
 		owner = msg.sender;
 	}
 
-	// トークンの送金
+	// トークンを転送する関数
 	function transfer(address to, uint256 amount) external {
+		// この関数を実行したアドレスの残高に指定したトークン量があるか確認
 		require(balances[msg.sender] >= amount, "Not enough balance");
 
+		// この関数を実行したアドレスの残高から指定したトークン量を引く
 		balances[msg.sender] -= amount;
 
+		// 指定したアドレスの残高に指定したトークン量を加える
 		balances[to] += amount;
 
+		// eventを発火
 		emit Transfer(msg.sender, to, amount);
 	}
 
+	// 残高を取得する関数
 	function balanceOf(address account) external view returns (uint256) {
 		return balances[account];
 	}
