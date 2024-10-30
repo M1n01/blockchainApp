5d383dabd4becab86258d853ed6571608bc94a72
M1n01 <abe.minato.bz@gmail.com>
2024-10-30 22:25:31 +0900

    complete 5.4

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  typechain: {
+    outDir: "frontend/types",
+    target: "ethers-v6",
+    alwaysGenerateOverloads: false, // コントラクトにおける関数のオーバーロードがない場合でも、"deposit(uint256)"のよう完全なシグネチャを生成するか
+    // externalArtifacts: ["externalArtifacts/*.json"], // Typeファイルの生成に追加したい外部のartifactsがある場合は指定する
+  }
 };
 
 export default config;
